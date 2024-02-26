7e37ed9454a46a8af21352c17fee2cb4473a362b
Eric <ebagnaro@gmail.com>
2024-02-26 13:43:37 +0100

    Initial Commit

---
+++ PomodoroFocus/PomodoroFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroFocusApp.swift
+//  PomodoroFocus
+//
+//  Created by Eric on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
